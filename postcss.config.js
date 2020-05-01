8153740d81a27114b606abc3371533a98cd9b677
Caleb Ely <le717@users.noreply.github.com>
2020-05-01 00:13:31 -0400

    Use twcss built-in purge command

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,14 @@
 const process = require("process");
-const tailwindcss = require("tailwindcss");
-const css_import = require("postcss-import");
-const autoprefixer = require("autoprefixer");
-const css_custom_props = require("postcss-custom-properties");
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/static/js/**/*.js", "./src/templates/**/*.html", "./src/templates/**/*.jinja2"],
-  css: ["./src/static/css/**/*.css"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
 const cssnano = require("cssnano")({
   preset: "default"
 });
 
 module.exports = {
   plugins: [
-    css_import,
-    tailwindcss,
-    css_custom_props,
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
-    autoprefixer,
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-custom-properties"),
+    require("autoprefixer"),
     ...(process.env.NODE_ENV === "production" ? [cssnano] : []),
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  purge: [
+    "./src/static/js/**/*.js",
+    "./src/templates/**/*.html",
+    "./src/templates/**/*.jinja2"
+  ],
   theme: {
     extend: {
       colors: {},
